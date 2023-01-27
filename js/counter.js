61aef46ca9e684e326c5d084099bdafd955f22b3
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2023-01-27 14:25:58 +0100

    initial commit

---
+++ js/counter.js
@@ -0,0 +1,17 @@
+// Global variable (with NULL coalescing: either take existing count, or start from 0)
+let count = +localStorage.getItem('count') ?? 0;
+            
+// Save the present count
+const saveCount = () => localStorage.setItem('count', count); // reset by replacing count by 0
+            
+// Fill in the html div element
+const updateCount = () => document.getElementById('media-counter').innerText = count + ' views';
+ 
+// Count one up, update count, and save global page count on this element
+const countUp = () => {
+      count = count + 1;
+      updateCount();
+      saveCount();
+      }
+            
+updateCount();
