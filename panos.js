7b67a37a34e66dd35ecfd5cc85669ee153ff86b6
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2023-01-29 18:06:47 +0100

    Add query string to panos for receiver pano

---
+++ panos.js
@@ -0,0 +1,10 @@
+
+			$(document).ready(function(){
+		   		$('#header').load("header.html");
+			});
+			
+			$(document).ready(function(){
+		   		$('#sidebar').load("sidebar.html");
+			});
+		
+
